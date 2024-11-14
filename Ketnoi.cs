439d8704f8005a8bb8b09853a4b895c5f266a052
12345trila <deobietok12@gmail.com>
2024-11-14 16:27:48 +0700

    Add files via upload
    22810310446

---
+++ Ketnoi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quanrlymaybay
+{
+    internal class Ketnoi
+    {
+        string conStr = @"Data Source=LAPTOP-2OPVKRUR\SQLEXPRESS;Initial Catalog=quản lý máy bay;Integrated Security=True";
+        SqlConnection conn;
+        public KetNoi()
+        {
+            conn = new SqlConnection(conStr);
+        }
+        public DataSet LayDuLieu(string truyvan)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter(truyvan, conn);
+                da.Fill(ds);
+                return ds;
+            }
+            catch
+            {
+                return null!;
+            }
+        }
+        public bool ThucThi(string truyvan)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(truyvan, conn);
+                int r = cmd.ExecuteNonQuery();
+                conn.Close();
+                return r > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
